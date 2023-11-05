38d5c44056933423c8574cf7c9046f54173078ec
boboskii <135865011+boboskii@users.noreply.github.com>
2023-11-06 06:47:10 +0800

    contract update

--- src/moonbasealpha/L165/L165.sol
+++ src/moonbasealpha/L165/L165.sol
@@ -7,116 +7,372 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 contract Lottery is IERC20, Ownable {
     IERC20 public token;
 
-    event BoughtTicket(address indexed from, uint16[9][] bets);
+
+    // Event
+    event BoughtTicket(address indexed from, uint16[] bets);
+    event claimed(address indexed from, uint256 indexed round,uint256 messageCode);//messageCode 0 succeeded ,1 no prize,2 reserveinsufficient
+    event winFP(address indexed from, uint256 indexed round,uint256 quantity);
+    event FPclaimed(address indexed from, uint256 indexed round,uint256 indexed quantity,string message);
     // Errors
-    // 错误，彩票合约专属事件。
-
     error PaymentFailed(uint256 ErrorCode);
+    error wait7days(address from,string message);
 
-    //
+
+
+    //NFT module
     mapping(uint256 => address) CouponOwner;
 
-    // Global Variables
-    // 全局变量
+        // constant
+    // 常量设置
+    uint16[14] public constant multiBetTicketsFactor=[1,1,1,1,1,6,21,56,126,252,462,792,1287,2002]; //彩票价格除数
+    uint16[5][9] public constant multiBetWinFactor=[
+            [5,0,2,4,3],
+            [10,10,3,12,6],
+            [15,30,4,24,10],
+            [20,60,5,40,15],
+            [25,100,6,60,21],
+            [30,150,7,84,28],
+            [35,210,8,112,36],
+            [40,280,9,144,45],
+            [45,360,10,180,55]
+    ]; //复投中奖数预计算
 
-    uint256 public pot = 0; // total amount of ether in the pot，奖池中的总金额，会计参数
-    uint256 public reserve = 0; // reserve fund for bonus payment，支付的储备基金，会计函数
-    uint256[] public bonus = [0]; // 当前轮可领取的奖金，初始值bonus[0]=0,在构造函数设定，会计函数
-    uint256 public fee = 0; // 手续费总额，会计函数
-    //彩票参数
-    uint256 public ticketPrice = 0; //彩票价格，the price of a ticket，会计参数
-    uint256 public startTime; // datetime that current round lottery is starting，当前轮开始的日期时间
-    uint256 public round = 0; // 彩票轮次
+    // accounting Variables 资金相关变量
+    uint32[3][] public historyDivisor=[[100,0,0xffffffff]]; //彩票价格除数历史[除数，起始轮次，结束轮次]
+    uint32 public Divisor=100; // 当前价格除数，价格为1/priceDec
+    uint256 public salesAmount = 0; // 当期销售额，
+    uint256 public currentFPpool = 0; // 当前头奖资金池
+    uint256 public currentLPpool = 0; // 当前低等级奖资金池
+    uint256 public reserve = 0; // 储备金库
+    uint256 public fee = 0; // 费用库
+    address public colletor;//fee 收集者
 
-    //可遍历的,用户参数
-    mapping(uint256 => address[]) players; // 参与轮次=>下注者地址
-    mapping(address => mapping(uint256 => uint16[9][])) buyRecord; // 某一轮的购买数组，[号码，注数][nonce]
+    uint256 public LPpoolTemp = [0,0,0]; // 头奖暂存池,256bit is nessesary,index->amount
+    uint256 public FPpoolTemp = [0,0,0,0,0,0,0]; // 头奖暂存池,256bit is nessesary,index->amount
 
-    mapping(uint256 => uint256) public firstPrizeBetAmount; // 头奖注数，mapping to store each round first prize winners amounts
-    mapping(uint256 => uint16[5]) winNumber; //mapping to store each rounds winning number，开盘结果，轮次=>号码
-    mapping(uint256 => uint256) CouponBonus; //mapping to store each rounds betAmmount，开盘结果，轮次=>总金额，统计函数,不储存,前端调用时计算
-    mapping(uint256 => address) CouponOwner; //mapping to store each rounds betAmmount，开盘结果，轮次=>总金额，统计函数,不储存,前端调用时计算
+    mapping(uint256 => uint256) LPpool; // 2,3等奖永久池 round->amount
+    mapping(uint256 => uint256) FPpool; // 1等奖永久池 round->amount
+    mapping(uint256 => uint256) CouponOwnerBonus; // NFT id->amount
+    mapping(uint256 => address) CouponOwneraddress; // NFT id->amount 
+  
+    uint256 public currentRound = 0; // 彩票轮次
+    uint16[] public winNumber; //彩票中奖号
+    uint32[] public FPquantitySum; //头奖总计票数
+    uint8 public status = 0; // 参数，
+    uint32[2][] public roundPeriod=[0,0xffffffff]; //彩票轮次时间段，[起始时间，结束时间]
+    mapping(address => mapping(uint256 => uint80[] )) ticketsRecord; // 用户彩票记录 address=>round =>ticketCode array
+    mapping(uint256 => address[]) FPwinner; // round->address winner record
+    mapping(address => mapping(uint256 => uint256)) FPquantityPlayer; // address->round winner record
+
 
     // 构造函数设置不同网络的AirnodeRrp合约地址，参数_airnodeRrpAddress
     constructor() {}
 
-    function startNewRound() external onlyOwner {
-        uint256 _baseFund = (reserve + pot);
-        reserve = _baseFund * 2 / 100; // 2% of the pot is reserved for bonus payment if bonus is not enough.
-        bonus.push(_baseFund * 80 / 100) = _baseFund * 80 / 100; // 80% of the pot is bonus
-        pot = _baseFund * 18 / 100; // 20% of the pot is reserved for the next round
-        round = round + 1;
-        startTime = block.timestamp;
+    /*//////////////////////////////////////////////////////////////  
+                            For DAO
+    //////////////////////////////////////////////////////////////*/
+
+  
+    function setPriceDivisor(uint32 _newPriceDivisor) external onlyOwner {
+        PriceDivisor[PriceDivisor.length-1][2]=round+1;//设置调整前价格除数数组
+        PriceDivisor.push([_newPriceDivisor,round,0xffffffff]);//设置调整后价格除数数组
+        Divisor=_newPriceDivisor; //设置当前价格除数
     }
 
-    // calldate struct: [0，1，2，3，4，5:tickets,6:coupon,7:id,8:isclaimed][]
+    function setColletor(address _newColletor) external onlyOwner {
+        colletor=_newColletor;
+    }
 
-    function buyTickets(uint16[9][] calldata bets_, uint256 calldata coupon_, bool calldata usecoupon_) external {
+     function depositFPpool(uint256 _round) external onlyOwner{
+        token.transferFrom(msg.sender,amount_);
+        FPpool[_round] += (amount_*priceDec);
+    }
+ 
+    /*//////////////////////////////////////////////////////////////  
+                            For Lottery logic
+    //////////////////////////////////////////////////////////////*/
+
+    function startNewRound() external onlyOwner {
+        //清算当前轮
+        //0.获取随机函数
+        //winNumber[currentRound]=random();
+        //if (!success ){random2()} //如果失败，再来一次,或后备随机数
+        //if (!success ){revert}
+        //1.清算低等奖金
+        uint256 l = LPpoolTemp[(currentRound+3)%3];//取出之前temp数据，    
+        LPpoolTemp[(currentRound+3)%3]=currentLPpool;//设置temp为当前轮数据，temp可以取钱，和fp不同。
+        currentLPpool=l*5/10;
+        reserve += l*5/10;
+
+        //2.清算头奖
+        //a. 当前轮大于6，7轮前有人中奖，7轮钱的fp暂存计入永久池。
+        if (FPquantitySum[currentRound-7]>0 ){
+            if (currentRound>6 ){
+                FPpool[currentRound-7]=FPpoolTemp[currentRound % 7]; //设置永久池数据，round-7轮，round 0-6时，暂存池为0值不变
+            }       
+            FPpoolTemp[(currentRound+7)%7]=currentFPpool; //暂存池已空，填入当前值数据，0轮即可，但需要+7，否则出错
+            currentFPpool=0;
+        }
+       //b. 当前轮大于6，7轮前无人中奖，永久池计入0。
+        if (FPquantitySum[currentRound-7]=0 ){
+            uint256 t = FPpoolTemp[currentRound % 7];//取出暂存池数据，
+          if (currentRound>6 ){
+               FPpool[currentRound-7]=0;// 设置永久池数据，round-7轮，第7轮开始才需要设置
+            }
+            FPpoolTemp[(currentRound+7)%7]=currentFPpool; //暂存池已空，填入当前值数据，0轮即可，但需要+7，否则出错
+            currentFPpool=t;//暂存池已空，填入缓存数据
+        }
+           
+        //3. 设置上一轮结束时间，当前轮开始时间，当前轮次加1
+        roundPeriod[currentRound-1][1]=[block.timestamp];
+        roundPeriod.push([block.timestamp,0xffffffff]);
+        ++currentRound;
+    }
+
+
+
+    function winnerClaim(uint256 _round) external  {
+        uint16 t;//ticket code
+        uint256 q;//ticket quantity
+        uint256 w;//winFP quantity
+        uint256 m;//mulitbet number
+        uint256 amount_=0;
+        uint80[] memory c = ticketsRecord[msg.sender][_round];//ticket number record
+
+        for(uint i = 0; i < c.length; ++i) {
+            t = uint16(c[i]>>64);
+            m = uint256((c[i]>>4) & 0xf);
+            q = uint256(c[i]>>40 & 0xffffff);
+             
+            if (m=0 ){
+                if (t == winNumbers[_round]){
+                    c[i] = c[i] | uint80(0x1101) ; //1101头奖claimed，状态更改
+                    w += q; //头奖总计票数   
+                } 
+
+                else {
+                    t = t & winNumbers[_round];
+                    if (checkOneAmount(t)==4){
+                    c[i] = c[i] | uint80(0x1001);//1001二奖claimed，状态更改
+                    amount_ += q*20/priceDec;
+                    }
+                    if (checkOneAmount(t)==3){
+                    c[i] = c[i] | uint80(0x0101);//0101三奖claimed，状态更改
+                    amount_ += q*2/priceDec;
+                    }
+             
+                }
+               
+            }//单式计算结束
+
+            if (m>0 ){
+                    t = t & winNumbers[_round];
+                    uint256 count=checkOneAmount(t);
+                    if (count==5){
+                        c[i] = c[i] | uint80(0x1101) ; //1101头奖claimed
+                        w += uint256(t & uint40(0xffffff)); //头奖总计票数 
+                        amount_ += q*20*multiBetWinFactor[m-6][0]/priceDec; //同时获得的二奖
+                        amount_ += q*2*multiBetWinFactor[m-6][1]/priceDec; //同时获得的三奖
+                    }
+
+                    if (checkOneAmount(t)==4){
+                        c[i] = c[i] | uint80(0x1001) ; //1001二奖claimed，状态更改
+                        amount_ += q*20*multiBetWinFactor[m-6][2]/priceDec; //同时获得的二奖
+                        amount_ += q*2*multiBetWinFactor[m-6][3]/priceDec; //同时获得的三奖
+                    }
+
+                    if (checkOneAmount(t)==3){         
+                        c[i] = c[i] | uint80(0x0101);//0101三奖claimed，状态更改
+                        amount_ += q*2*multiBetWinFactor[m-6][4]/priceDec; //同时获得的三奖
+                    }    
+                        
+            }//复式计算结束
+
+        }//for循环结束，最终支付阶段
+
+        ticketsRecord[msg.sender][_round] = c;
+        if (w > 0 ){
+            FPquantitySum[_round] += w; //头奖总计票数
+            FPwinner[_round].push(msg.sender);//头奖计入中奖者地址
+            FPquantityPlayer[msg.sender][_round] = w; //个人头奖总计票数
+            emit winFP(msg.sender,round,w); //发出事件
+        }
+        
+        if (amount_ == 0 ){
+        emit claimed(msg.sender,round,1);//messageCode 0 succeeded ,1 no prize,
+        }
+        else {
+            if (LPpoolTemp[(currentRound+3)%3]>amount_){
+                token.transfer(msg.sender,amount_);
+                LPpoolTemp[(currentRound+3)%3] -= amount_;
+                emit claimed(msg.sender,round,0);//messageCode 0 succeeded
+                }
+            else if (reserve >amount_ ){
+                token.transfer(msg.sender,amount_);
+                reserve -= amount_;
+                emit claimed(msg.sender,round,0);//messageCode 0 succeeded
+                }
+            emit claimed(msg.sender,round,2);//messageCode 0 succeeded ,1 no prize,2 reserveinsufficient
+          }
+
+    }
+
+    function FPclaim(uint256 _round) external  {
+        if (_round-8<0){
+            revert(msg.sender,"please wait 7 days for otherplayer claim");
+        }
+        address c = FPwinner[_round];
+        for (uint256 i = 0; i < FPwinner[_round].length; i++) {
+            if (a[i]==msg.sender){
+                uint256 amount_=FPpool[_round]*(FPquantityPlayer[msg.sender][_round]/(FPquantitySum[_round])/priceDec);
+                FPpool[_round]-=amount_;
+                token.transfer(msg.sender,amount_);
+                emit FPclaimed(msg.sender, _round,amount_,"u win");//messageCode 0 succeeded
+            }
+        }
+
+    }
+
+    function buyTickets(uint40[] calldata bets_, uint256 calldata coupon_) external {
         uint256 _amount = 0;
         uint256 i;
-        for (i = 0; i < bets_.length; i++) {
-            if (
-                bets_.length < 11 && bets_[i][5] > 0 && bets_[i][0] < bets_[i][1] && bets_[i][1] < bets_[i][2]
-                    && bets_[i][2] < bets_[i][3] && bets_[i][3] < bets_[i][4]
-            ) {
-                //coupon 发行号1001-1999
-                if (usecoupon_) {
-                    _amount = (bets_[i][5] + _amount) * ticketPrice * 97 / 100;
-                    buyRecord[msg.sender][round].push(bets_[i]);
-                    pot += _amount * 90 / 97;
+        uint80 memory t;
+        uint256 c;
+        uint256 q;
+           for (i = 0; i < bets_.length; i++) {
+             //coupon 发行号1001-1999
+                if (coupon_>0) {
+                    q = bets_[i] & uint40(0xffffff);
+                    t = uint80((bets_[i]<<32 | uint72(block.timestamp)<<8)); //运算优先级，还需要检查
+                    c = checkOneAmount(bets_[i]);
+                    t = t | uint80(c<<2);//构造票据储存格式
+                    ticketsRecord[msg.sender][currentRound].push(t);
+                    _amount = (q/Divisor) * (97 / 100);
+                    currentLPpool +=
+                    currentFPpool +=
                     fee += _amount * 4 / 97;
-                    CouponBonus[bets_[i][6]] += _amount * 3 / 97;
+                    reserve += 
+                    CouponOwnerBonus[coupon_] += _amount * 3 / 97;
                 } else {
-                    _amount += bets_[i][5] * ticketPrice;
-                    buyRecord[msg.sender][round].push(bets_[i]);
-                    pot += _amount * 93 / 100;
-                    fee += _amount * 7 / 100;
+                    q = bets_[i] & uint40(0xffffff);
+                    t = uint80((bets_[i]<<32 | uint72(block.timestamp)<<8)); //运算优先级，还需要检查
+                    c = checkOneAmount(bets_[i]);
+                    t = t | uint80(c<<2);//构造票据储存格式
+                    ticketsRecord[msg.sender][currentRound].push(t);
+                    _amount = (q/Divisor) * (97 / 100);
+                    currentLPpool +=
+                    currentFPpool +=
+                    fee += _amount * 4 / 97;
+                    reserve += ;
                 }
-            } // if条件语句结束
-            else {
-                revert BetdataNotStandard(2);
-            }
-        } // for循环结束
+           
+
+            } // for循环结束
+
         if (token.transferFrom(msg.sender, address(this), _amount)) {
             emit BoughtTicket(msg.sender, buyRecord[msg.sender][round]);
-            players[round].push(msg.sender);
         } else {
             revert PaymentFailed(2);
         }
     }
 
+  function claimBonus(uint256 calldata claimRound_) external {
+
+      mapping(uint256 => uint256) CouponOwnerBonus; // NFT id->amount
+    mapping(uint256 => address) CouponOwneraddress; // NFT id->amount 
+
+    }
+   
+
+    /*//////////////////////////////////////////////////////////////  
+                            Free Function
+    //////////////////////////////////////////////////////////////*/
+    
+    function winNumberCompare(uint16 ticket,uint256 _round) external returns(uint) {
+    uint n;
+    n = ticket & winNumber(_round);
+    return n=checkOneAmount(uint16(n));
+    }
+
+    function checkOneAmount(uint16  _ticketNumber) public pure returns(uint256){
+        uint256 count_=0;
+        do {
+            _ticketNumber &= _ticketNumber-1;
+            ++count_;
+        } 
+        while (_ticketNumber !=0);
+        return count_;
+    }
+
+    function winNumberPackedcodeToArray(uint16 _ticketNumber) public pure returns(uint256[5] memory){
+        uint256[5] memory winNumberArray_;
+        uint256 j=0;
+        for(uint i = 0; i < 16; ++i) {
+            if (_ticketNumber & (1 << i ) != 0){
+                winNumberArray_[j] = 16-i;
+                ++j;
+            }
+ 
+        }
+        return winNumberArray_;
+    }
+
+    function winNumberArrayToPackedcode(uint256[5] memory _winNumberArray) public pure returns(uint16){
+        uint16 winNumberPackcode_;
+        for(uint i = 0; i < 5; ++i) {
+            winNumberPackcode_ |= (1 << _winNumberArray[i]);
+        }
+        return winNumberPackcode_;
+    }
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
     //  mapping(uint256 => address[]) players; // 参与轮次=>下注者地址
     //  mapping(address => mapping(uint256 => uint[6][])) buyRecord; // 某一轮的购买数组，[5号码，6注数][nonce]
     // calldate struct: [uint256 _round,uint256 _nonce][uint256 _times]
-    function claimBonus(uint256 calldata claimRound_) external {
-        address _player = msg.sender;
-        uint256 memory _amount = 0;
-        uint256[][] memory _betdata = buyRecord[_player][claimRound_];
-        for (uint256 i = 0; i < _betdata.length; i++) {
-            iswin();
-        }
-
-        uint256 balance = token.balanceOf(address(this));
-        require(token.transfer(owner, balance), "transfer failed");
-    }
+  
 
     // mapping(uint256 => address[]) players; // 参与轮次=>下注者地址
     // 遍历一期的所有购买地址，计算头奖总注数，
 
     function firstPrizeBetAmountCheck(uint256 calldata round_) public view returns (uint256) {
         uint8[5] memory _code;
-        address[] _players = players[round]; // 本期下注者地址
-        uint256 memory _amount = 0; // 本期头奖总注数
-        uint256[5][] memory _betdata = buyRecord[_players][round_]; //每个下注者的购买数据
 
-        for (uint256 i = 0; i < _players.length; i++) {
-            for (uint256 j = 0; j < _betdata[1][i]; j++) {
-                _code = [_betdata[0][j], _betdata[1][j], _betdata[2][j], _betdata[3][j], _betdata[4][j]];
-                if (_code == winNumber[round]) {
-                    firstPrizeBetAmount[round] += _betdata[5][j]; //[5][i]是注数
-                }
-            }
-            return firstPrizeBetAmount[round];
-        }
     }
+
+ 
+
+    function ticketVerify(uint40[] memory _ticketArray) public  returns (bool)  {
+          
+        for (uint i = 0; i < ticketArray.length; ++i) {
+            if (ticketCode[i] == 0) {
+                return false;
+            }
+            if (ticketCode[i] == 0) {
+                return false;
+            }
+
+        } 
+    }
+    
+
+
+   
+    
+
+
+
 }
--- src/moonbasealpha/L165/Ticket.sol
+++ src/moonbasealpha/L165/Ticket.sol
@@ -3,86 +3,25 @@
 pragma solidity ^0.8.9;
 
 contract Ticket {
-    // Events
-    
-    event NoPrize(address indexed from, uint256 round);
 
-    // Custom errors
-    error InvalidRounds();
-    error InvalidTicket(uint256 errorCode);
 
-    // testing variables
-    uint public c=0;
-    uint16 public numbers=0;
-    uint[5] public  win = [1,2,3,4,5];
-   
-    // constant
-    // 常量设置
+
     uint16[14] public constant multiBetTicketsFactor=[1,1,1,1,1,6,21,56,126,252,462,792,1287,2002]; //彩票价格除数
     uint16[5][9] public constant multiBetWinFactor=[
-            [5, 0, 2, 4, 3],
-            [10, 10, 3, 12, 6],
-            [15, 30, 4,	24,	10],
-            [20, 60, 5,	40,	15],
-            [25, 100, 6, 60, 21],
-            [30, 150, 7, 84, 28],
-            [35, 210, 8, 112, 36],
-            [40, 280, 9, 144, 45],
-            [45, 360, 10, 180, 55]
-    ]; //复投中奖数预计算
-
-    // 可设置模块，需要owner模块，未完成
-    function setPriceDivisor(uint32 _newPriceDivisor) external onlyOwner {
-        PriceDivisor[PriceDivisor.length-1][2]=round+1;//设置调整前价格除数数组
-        PriceDivisor.push([_newPriceDivisor,round,0xffffffff]);//设置调整后价格除数数组
-        currentPriceDivisor=_newPriceDivisor; //设置当前价格除数
-    }
-
- 
-
-
-    uint32[3][] public PriceDivisor=[[100,0,0xffffffff]]; //彩票价格除数历史[除数，起始轮次，结束轮次]
-    uint32 public currentPriceDivisor=100; // 当前价格除数，价格为1/priceDec
-    uint32 public round = 0;        //轮次
-    uint16[] public winNumbers=[0]; //中奖号码
-    mapping(address => mapping(uint256 => uint80[] )) ticketsRecord; // address=>round =>ticketCode array
-    mapping(uint256 => mapping(address => uint256 )) firstPriceRecord;//轮次 赢家 票数
+            [5,0,2,4,3],
+            [10,10,3,12,6],
+            [15,30,4,24,10],
+            [20,60,5,40,15],
+            [25,100,6,60,21],
+            [30,150,7,84,28],
+            [35,210,8,112,36],
+            [40,280,9,144,45],
+            [45,360,10,180,55]
+    ]
 
 
 
-    // library
-    function checkWinNumberAmount(uint16  _ticketNumber) public pure returns(uint256){
-        uint256 count_=0;
-        do {
-            _ticketNumber &= _ticketNumber-1;
-            ++count_;
-        } 
-        while (_ticketNumber !=0);
-        return count_;
-    }
- 
     
-    //--j 发生下溢，不知解决方案，倒序问题前端再解决吧，
-    function winNumberPackedcodeToArray(uint16 _ticketNumber) public pure returns(uint256[5] memory){
-        uint256[5] memory winNumberArray_;
-        uint256 j=0;
-        for(uint i = 0; i < 16; ++i) {
-            if (_ticketNumber & (1 << i ) != 0){
-                winNumberArray_[j] = 16-i;
-                ++j;
-            }
- 
-        }
-        return winNumberArray_;
-    }
-
-    function winNumberArrayToPackedcode(uint256[5] memory _winNumberArray) public pure returns(uint16){
-        uint16 winNumberPackcode_;
-        for(uint i = 0; i < 5; ++i) {
-            winNumberPackcode_ |= (1 << _winNumberArray[i]);
-        }
-        return winNumberPackcode_;
-    }
 
 
     function ticketBuyingCheck(uint40[] memory ticketArray) public  returns (bool)  {
@@ -129,73 +68,9 @@ contract Ticket {
 
 
 
- function winnerClaim(uint256 _choosedround) external  {
-        uint16 ticketNumber;
-        uint256 amount_=0;
-        uint80[] memory copy = ticketsRecord[msg.sender][_choosedround];
-
-        for(uint i = 0; i < copy.length; ++i) {
-            ticketNumber=uint16(copy[i]>>64);
-
-            if (ticketNumber == winNumbers[0]){
-                copy[i] | uint80(1<<40) ;
-                firstPriceRecord[_choosedround][msg.sender]=(copy[i]>>48)&0xffff;
-                } 
-
-            else {
-                ticketNumber = ticketNumber & winNumbers[_choosedround];
-                if (checkWinNumberAmount(ticketNumber)==4){
-                   amount_ += ((copy[i]>>48) & 0xffff)*20/priceDec;
-                }
-                if (checkWinNumberAmount(ticketNumber)==3){
-                   amount_ += ((copy[i]>>48) & 0xffff)*2/priceDec;
-                }
-         
-              
-            }
-    
-        }
-        ticketsRecord[msg.sender][_choosedround]=copy;
-        if (amount_ == 0){
-        emit NoPrize(msg.sender,round);
-        }
-        else {
-            //token.transfer(msg.sender,amount_);
-        }
-    }
+ 
 
 
 
-
-
-
-    function random(uint d) public view returns (uint) {
-        return uint(keccak256(abi.encodePacked(block.timestamp,d)));
-    }
-
-    function generate() public  returns(uint[5] memory,uint16) {
-        uint[5] memory n;
-        uint t=0;
-        for(uint i = 0; i < 5; ++i) {
-            n[i] = uint16(random(i) % 16 + 1);
-        }
-        win = n;
-
-        for  (uint j = 0; j < 5; ++j) {
-             t= t | (1 << n[j]);
-        }
-        numbers = uint16(t);
-        
-       return (win,numbers);
-
-    }
-
-  
-
-    function compare(uint16 ticket) external returns(uint) {
-    uint n;
-    n = ticket & numbers;
-    return n=checkWinNumberAmount(uint16(n));
-}
 
 }
