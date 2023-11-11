c15709a5b5e7afe6369ea231018fecb39c58c626
boboskii <135865011+boboskii@users.noreply.github.com>
2023-11-12 00:26:08 +0800

    nft init

--- src/moonbasealpha/Slot/ZSM.sol
+++ src/moonbasealpha/Slot/ZSM.sol
@@ -6,27 +6,38 @@ import "solmate/auth/Owned.sol";
 
 contract ZSM is ERC4626, Owned {
 
-    mapping(address => uint256) public shareHolder;//4626,shareHolder
-
-    uint128 public fees ; // 费用库
+    //4626 accounting viarables
+    uint256 public shareSum;
+    uint256 public fees ; // 费用库
     address public collector;
+    mapping(address=>uint256) shareHolder;
 
-    uint256 public contractBalance;
-    uint256 round = 0;
-    uint8[3] speed0 = [31, 51, 71];
+    //slot viarables
+    uint64[3] randomNums;
     uint8 acceleration = 10;
+    uint256 bonus;
+    uint8[3][] result;
 
-    event Result(address indexed from, uint256 timestamp, string message);
-    event result(uint256[], uint256[], uint256);
-    event balances(uint256 betAmount, uint256 userBalance, uint256 contractBalance);
+    event Result(address indexed from,uint8[3][] indexed result, uint256 bonus);
+    error NotEnoughfeess();
+
 
     constructor(
         ERC20 _asset,
         string memory _name,
-        string memory _symbol
-    ) ERC4626(_asset, _name, _symbol) {
-
+        string memory _symbol,
+        address _owner
+    ) ERC4626(_asset, _name, _symbol) Owned(_owner) {
+        _asset = address(0x0);
+        _name = "Zeno Slot Machine Shares-MB-VDOT";
+        _symbol = "ZS-MB-VDOT";
+        _owner = msg.sender;
     }
+
+
+    /*//////////////////////////////////////////////////////////////
+                            ZENO FEES LOGIC
+    //////////////////////////////////////////////////////////////*/
 
     modifier onlyCollector() {
         if (msg.sender != collector) {
@@ -35,124 +46,94 @@ contract ZSM is ERC4626, Owned {
         _;
     }
 
-
-    function withDrawFees() external onlyCollector returns (bool) {
+    function withDrawFees() external onlyCollector returns (bool, uint256) {
         if (fees > 10) {
             asset.transfer(collector, fees);
+            return (true, fees);
             fees = 0;
-            return true;
         } else {
             revert(NotEnoughfeess());
-            return false;
+            return (false, fees);
         }
     }
 
-// 4626
-    function _deposit(uint _assets) public {
-    // checks that the deposited amount is greater than zero.
-    require(_assets > 0, "Deposit less than Zero");
-    // calling the deposit function ERC-4626 library to perform all the functionality
-    deposit(_assets, msg.sender);
-    // Increase the share of the user
-    shareHolder[msg.sender] += _assets;
-}
+    /*//////////////////////////////////////////////////////////////
+                            VAULT LOGIC
+    //////////////////////////////////////////////////////////////*/
 
-   // 4626 returns total number of assets
+    // 4626 returns total number of assets
     function totalAssets() public view override returns (uint256) {
     return asset.balanceOf(address(this));
     } 
 
-    // returns total balance of user ???
-    function totalAssetsOfUser(address _user) public view returns (uint256) {
-    return asset.balanceOf(_user);
-    }
-
-    function totalShareOfUser(address _user) public view returns (uint256) {
-    return asset.balanceOf(_user);
+    /**
+    * @notice Function to allow msg.sender to withdraw their deposit plus accrued interest
+    * @param _assets amount of assets the user wants to deposit
+    */
+    function _deposit(uint _assets) public {
+    require(_assets > 0, "Require greater than Zero");
+    // calling the deposit function ERC-4626 library to perform all the functionality
+    deposit(_assets, msg.sender);
+    shareHolder[msg.sender] += _shares;
+    // Increase the share of the user
     }
 
     /**
     * @notice Function to allow msg.sender to withdraw their deposit plus accrued interest
     * @param _shares amount of shares the user wants to convert
-    * @param _receiver address of the user who will receive the assets
     */
-    function _withdraw(uint _shares, address _receiver) public {
-    // checks that the deposited amount is greater than zero.
-    require(_shares > 0, "withdraw must be greater than Zero");
-    // Checks that the _receiver address is not zero.
-    require(_receiver != address(0), "Zero Address");
-    // checks that the caller is a shareholder
-    require(shareHolder[msg.sender] > 0, "Not a shareHolder");
-    // checks that the caller has more shares than they are trying to withdraw.
-    require(shareHolder[msg.sender] >= _shares, "Not enough shares");
-    // Calculate 10% yield on the withdraw amount
-    uint256 percent = (2 * _shares) / 1000;
-    // Calculate the total asset amount as the sum of the share amount plus 10% of the share amount.
-    uint256 assets = _shares + percent;
-    // calling the redeem function from the ERC-4626 library to perform all the necessary functionality
-    redeem(assets, _receiver, msg.sender);
-    // Decrease the share of the user
-    shareHolder[msg.sender] -= _shares;
-
+    function _withdraw(uint _shares) public {
+    require(_shares > 0, "Require greater than 0");
+    require(balanceOf[msg.sender] > 0, "Not a shareHolder");
+    require(balanceOf[msg.sender] >= _shares, "Not enough shares");
+    redeem(_shares, msg.sender, msg.sender);
+    shareHolder[msg.sender] -= _shares; // 写shareHolder，supply，balanceOf三个储存
     }
 
+    /*//////////////////////////////////////////////////////////////
+                            SLOTMACHINE LOGIC
+    //////////////////////////////////////////////////////////////*/
 
+    
+    function insertionSort(uint256[] memory a) public pure returns(uint256[] memory) {
+        // note that uint can not take negative value
+        for (uint i = 1;i < a.length;i++){
+            uint temp = a[i];
+            uint j=i;
+            while( (j >= 1) && (temp < a[j-1])){
+                a[j] = a[j-1];
+                j--;
+            }
+            a[j] = temp;
+        }
+        return(a);
+    }
+
+    function random(uint256 number) public view returns(uint256[]) {
+        for (uint256 i; i < number; ++i) {
+            randomNums[i] = uint256(keccak256(abi.encodePacked(block.timestamp, block.difficulty,i)));
+        }
+        return insertionSort(randomNums);
+    }
 
     function play(uint256 _betAmount) public payable {
         // Uncomment below code once dummy user has balance
     }
 
-    function logicOfLuck(uint256 _betAmount) public payable {
-        uint8 reel1 = random();
-        uint8 reel2 = random();
-        uint8 reel3 = random();
-        emit Nums(reel1, reel2, reel3);
-        bool isWin = false;
-        uint256 prizeAmount = 0 wei;
-
-        if (reel1 == reel2 || reel2 == reel3 || reel3 == reel1) {
-            // Check if all same
-            if (reel1 == reel2 && reel2 == reel3) {
-                prizeAmount = _betAmount * 3 wei;
-                console.log(prizeAmount);
-                emit Result(msg.sender, block.timestamp, "JACKPOT! You tripled your ETH bet");
-            } else {
-                emit Result(msg.sender, block.timestamp, "Congratulations. You doubled your ETH bet");
-                prizeAmount = _betAmount * 2 wei;
-                console.log(prizeAmount);
-            }
-            isWin = true;
-        } else {
-            emit Result(msg.sender, block.timestamp, "Loser. :)");
-        }
-
-        if (isWin) {
-            require(prizeAmount <= address(this).balance, "Trying to withdraw more money than the contract has.");
-            (bool success,) = (msg.sender).call{value: prizeAmount}("");
-            require(success, "Failed to withdraw money from contract.");
-        }
-    }
-
-    //单次下注
+     //单次下注
     function betOnce() public returns (uint256) {}
 
     //多次下注
     function betMulti() public returns (uint256) {}
 
-    //随机函数
-    function randomT() public returns (uint256) {
-        uint256 t = uint256(keccak256(abi.encodePacked(block.timestamp))) % 20;
-        round++;
-        return t;
-    }
-
+ 
     //慢雾安全团队建议开发者们不要将用户的下注与开奖放在同一个交易内
-    //防止攻击者通过检测智能合约中的开奖状态实现交易回滚攻击。
-    function random() public returns (uint256[] memory, uint256[] memory, uint256) {
+    function Bets(uint256 _betAmount,uint256 _rounds) public returns (uint256[] memory, uint256[] memory, uint256) {
         uint256 bonusRate = 0;
         uint256[] memory slot = new uint[](3);
         uint256[] memory step = new uint[](3);
         uint256 t = randomT();
+        random(3);
 
         for (uint256 i = 0; i < 3; i++) {
             step[i] =
